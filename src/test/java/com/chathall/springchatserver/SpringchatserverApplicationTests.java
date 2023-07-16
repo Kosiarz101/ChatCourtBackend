e4c120fc465dcefe4b17f9f560301c17e80d6a5b
Kosiarz101 <pol1100@o2.pl>
2023-07-16 07:37:42 +0200

    project init

---
+++ src/test/java/com/chathall/springchatserver/SpringchatserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chathall.springchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringchatserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
